b6666b2bfbd64a336db8a095f36165eb14cad613
KDV777 <k-d@bk.ru>
2023-03-19 17:08:42 +0500

    lesson_12 finish

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -93,6 +93,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'admin',
+        'PASSWORD': '123',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
